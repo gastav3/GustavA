ee9a327aaea09ef1fa7a04195af2b8c7922d2695
gastav3 <gastav3@hotmail.com>
2018-04-07 23:56:27 +0200

    added view 4 and updated the database file

---
+++ queries.sql
@@ -0,0 +1,13 @@
+-- Utgå ifån att datumet är 2018-04-04
+
+-- Uppgift 1
+SELECT * FROM  all_movies_view;
+
+-- Uppgift 2
+SELECT * FROM all_movies_with_genre_action;
+
+-- Uppgift 3
+SELECT * FROM all_movies_that_are_lended_out_view;
+
+-- Uppgift 4
+SELECT * FROM all_movies_that_should_have_been_returned_view;
